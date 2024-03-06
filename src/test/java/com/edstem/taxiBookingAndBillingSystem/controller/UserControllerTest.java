d3babd55d3a29fb71a2c8e75b151398786fedb28
prashobhpadmanabhan <119028182+prashobhpadmanabhan@users.noreply.github.com>
2024-03-06 11:20:36 +0530

    tests passing

--- src/test/java/com/edstem/taxiBookingAndBillingSystem/controller/UserControllerTest.java
+++ src/test/java/com/edstem/taxiBookingAndBillingSystem/controller/UserControllerTest.java
@@ -9,8 +9,8 @@ import com.edstem.taxiBookingAndBillingSystem.contract.response.UpdateAccountRes
 import com.edstem.taxiBookingAndBillingSystem.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
